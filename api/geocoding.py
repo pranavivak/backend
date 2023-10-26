39eda5c5ef0daa95b1f2ec0d8e312ad73191c764
hanr12 <hannelarey@gmail.com>
2023-10-26 00:10:33 -0700

    debugging

--- api/geocoding.py
+++ api/geocoding.py
@@ -7,10 +7,10 @@ def miles_to_meters(miles):
         return miles + 1_609.344
     except:
         return 0
-API_KEY = OPEN('API_KEY.txt','r').read()
+API_KEY = AIzaSyDwh-rt_rOBI3qhUZwaFiHN0Qba4zyVZwc
 map_client = googlemaps.Client(API_KEY)
 
-location = (33.015057786476575, -117.12225067107188)
+location = (API_KEY)
 search_string = 'art'
 distance = miles_to_meters(15)
 business_list = []
@@ -37,7 +37,7 @@ while next_page_token:
     business_list.extend(response.get('results'))
     next_page_token = response.get('next_page_token')
     
-df = pd.DataFeane(business_list)
+df = pd.DataFrame(business_list)
 df['url'] = 'https://www.google.com/maps/place?q=place_id' + df['place_id']
 df.to_excel('art museum list.xlsx',index=False)
 
