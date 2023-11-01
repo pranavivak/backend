ca042b6b04097ba7b6de14e5752519a2fa3bdd94
pranavivak <pranavi.vakkala15@gmail.com>
2023-11-01 12:54:20 -0700

    fixing the fuction for geocoding model

--- api/geocoding.py
+++ api/geocoding.py
@@ -9,21 +9,8 @@ geocoding_api = Blueprint('geocoding_api', __name__,
                            url_prefix='/api/geocoding')
 # API generator
 api = Api(geocoding_api)
-api_key = 'AIzaSyDwh-rt_rOBI3qhUZwaFiHN0Qba4zyVZwc'
 class GeocodingAPI:
     class _Read(Resource):
-        def get(zipcode):
-            try:
-                return 'No results found for ' + zipcode
-            except:
-                return 0
-        def post(data):
-            map_client = googlemaps.Client(api_key)
-            search_string = 'art museums near'
-            response = map_client.textsearch(
-                query=search_string,
-                fields = ["formatted_address","name","rating","opening_now"],
-                )
-            results = response.get('results')
-            return jsonify (results)
+        def get(self):
+            return jsonify(getPlaces(zip))
 api.add_resource(GeocodingAPI._Read, '/')
--- model/geocoding.py
+++ model/geocoding.py
@@ -1,7 +1,17 @@
-import json 
-
-def initGeocoding():
-      results=[]
+import json
+import googlemaps
+def initGeocoding(self,zipcode):
+   self.zip = zipcode
+def getPlaces(self,zip):
+   api_key = 'AIzaSyDwh-rt_rOBI3qhUZwaFiHN0Qba4zyVZwc'
+   map_client = googlemaps.Client(api_key)
+   search_string = 'art museums near'+ " "+zip
+   response = map_client.textsearch(
+      query=search_string,
+      fields = ["formatted_address","name","rating","opening_now"],
+      )
+   results = response.get('results')
+   return (results)
 
 
 
