d39922443846d713575e599ef01741b07f3534d5
pranavivak <pranavi.vakkala15@gmail.com>
2023-11-01 13:04:29 -0700

    editing model

--- model/geocoding.py
+++ model/geocoding.py
@@ -5,7 +5,7 @@ def initGeocoding(self,zipcode):
 def getPlaces(self,zip):
    api_key = 'AIzaSyDwh-rt_rOBI3qhUZwaFiHN0Qba4zyVZwc'
    map_client = googlemaps.Client(api_key)
-   search_string = 'art museums near'+ " "+zip
+   search_string = 'art museums near'+ zip
    response = map_client.textsearch(
       query=search_string,
       fields = ["formatted_address","name","rating","opening_now"],
