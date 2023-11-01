1649f3226faa6dca52d1f9e24c2d162e62d1a85d
pranavivak <pranavi.vakkala15@gmail.com>
2023-11-01 14:37:16 -0700

    testing zip code with hard coded value

--- model/geocoding.py
+++ model/geocoding.py
@@ -3,9 +3,10 @@ import googlemaps
 def initGeocoding(self,zipcode):
    self.zip = zipcode
 def getPlaces(self,zip):
+   zip = 92127
    api_key = 'AIzaSyDwh-rt_rOBI3qhUZwaFiHN0Qba4zyVZwc'
    map_client = googlemaps.Client(api_key)
-   search_string = 'art museums near'+ zip
+   search_string = 'art museums near'+ 'zip'
    response = map_client.textsearch(
       query=search_string,
       fields = ["formatted_address","name","rating","opening_now"],
